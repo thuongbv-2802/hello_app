78b02f479ae470d663788b6cc060a309039a0f20
buivanthuong <bui.van.thuong@sun-asterisk.com>
2021-11-10 10:42:27 +0700

    end project/ follow user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
     # đảm bảo micropost bị xóa cùng ng dùng
     has_many :microposts, dependent: :destroy
-    attr_accessor :remember_token,  :activation_token,  :reset_token
+    has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+    has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+    has_many :following, through: :active_relationships, source: :followed
+    has_many :followers, through: :passive_relationships, source: :follower
+    # method getter/setter 
+    attr_accessor :remember_token,  :activation_token, :reset_token
     before_save   :downcase_email
     before_create :create_activation_digest
     VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -69,11 +74,31 @@ class User < ApplicationRecord
         UserMailer.password_reset(self).deliver
     end
 
+    # Returns true if a password reset has expired.
+    def password_reset_expired?
+        reset_sent_at < 2.hours.ago
+    end
+
     # Defines a proto-feed.
     # See "Following users" for the full implementation.
     def feed
-        # ? đảm bảo rằng id được thoát đúng cách trước khi được đưa vào truy vấn SQL cơ bản
-        Micropost.where("user_id = ?", id)
+        following_ids = "SELECT followed_id FROM relationships WHERE  follower_id = :user_id"
+        Micropost.where("user_id IN (:following_ids) OR user_id = :user_id", following_ids: following_ids, user_id: id)
+    end
+
+    # Follows a user.
+    def follow(other_user)
+        active_relationships.create(followed_id: other_user.id)
+    end
+
+    # Unfollows a user.
+    def unfollow(other_user)
+        active_relationships.find_by(followed_id: other_user.id).destroy
+    end
+
+    # Returns true if the current user is following the other user.
+    def following?(other_user)
+        following.include?(other_user)
     end
 
     private
--- config/routes.rb
+++ config/routes.rb
@@ -4,15 +4,22 @@ Rails.application.routes.draw do
   get 'password_resets/edit'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
+
   delete 'logout', to: 'sessions#destroy'
+
   get 'home', to: 'static_pages#home'
   get 'help', to: 'static_pages#help'
   get 'about', to: 'static_pages#about'
   get 'contact', to: 'static_pages#contact'
   get 'signup', to: 'users#new'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
   root 'static_pages#home'
 end
